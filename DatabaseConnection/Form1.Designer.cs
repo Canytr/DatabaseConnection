281c85f08fa551f06b453cdaf727f5c71e578e74
Can YETER <cannytr@gmail.com>
2024-09-06 10:33:16 +0300

    Add log messages Run sql command Add table and delete table

--- DatabaseConnection/Form1.Designer.cs
+++ DatabaseConnection/Form1.Designer.cs
@@ -34,15 +34,20 @@
             this.btnConnect = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.labelSelectTable = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelScript = new System.Windows.Forms.Label();
             this.btnExecute = new System.Windows.Forms.Button();
+            this.richTextBoxSql = new System.Windows.Forms.RichTextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnAddTable = new System.Windows.Forms.Button();
+            this.btnDeleteTable = new System.Windows.Forms.Button();
+            this.labelLog = new System.Windows.Forms.Label();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // txtConnectionString
             // 
-            this.txtConnectionString.Location = new System.Drawing.Point(212, 44);
+            this.txtConnectionString.Location = new System.Drawing.Point(214, 21);
             this.txtConnectionString.Name = "txtConnectionString";
             this.txtConnectionString.Size = new System.Drawing.Size(485, 20);
             this.txtConnectionString.TabIndex = 0;
@@ -51,7 +56,7 @@
             // 
             this.labelConnectionString.AutoSize = true;
             this.labelConnectionString.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelConnectionString.Location = new System.Drawing.Point(59, 44);
+            this.labelConnectionString.Location = new System.Drawing.Point(61, 21);
             this.labelConnectionString.Name = "labelConnectionString";
             this.labelConnectionString.Size = new System.Drawing.Size(147, 17);
             this.labelConnectionString.TabIndex = 2;
@@ -60,7 +65,7 @@
             // cmbTables
             // 
             this.cmbTables.FormattingEnabled = true;
-            this.cmbTables.Location = new System.Drawing.Point(212, 135);
+            this.cmbTables.Location = new System.Drawing.Point(214, 96);
             this.cmbTables.Name = "cmbTables";
             this.cmbTables.Size = new System.Drawing.Size(121, 21);
             this.cmbTables.TabIndex = 8;
@@ -69,7 +74,7 @@
             // 
             this.btnConnect.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnect.Location = new System.Drawing.Point(721, 41);
+            this.btnConnect.Location = new System.Drawing.Point(723, 18);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(117, 39);
             this.btnConnect.TabIndex = 9;
@@ -81,35 +86,27 @@
             // 
             this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(62, 192);
+            this.dataGridView.Location = new System.Drawing.Point(62, 123);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(776, 227);
+            this.dataGridView.Size = new System.Drawing.Size(776, 209);
             this.dataGridView.TabIndex = 10;
             // 
             // labelSelectTable
             // 
             this.labelSelectTable.AutoSize = true;
             this.labelSelectTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSelectTable.Location = new System.Drawing.Point(62, 135);
+            this.labelSelectTable.Location = new System.Drawing.Point(64, 96);
             this.labelSelectTable.Name = "labelSelectTable";
             this.labelSelectTable.Size = new System.Drawing.Size(109, 17);
             this.labelSelectTable.TabIndex = 11;
             this.labelSelectTable.Text = "Select Table :";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(62, 481);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(380, 57);
-            this.textBox1.TabIndex = 12;
             // 
             // labelScript
             // 
             this.labelScript.AutoSize = true;
             this.labelScript.BackColor = System.Drawing.Color.Transparent;
             this.labelScript.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelScript.Location = new System.Drawing.Point(59, 451);
+            this.labelScript.Location = new System.Drawing.Point(59, 353);
             this.labelScript.Name = "labelScript";
             this.labelScript.Size = new System.Drawing.Size(91, 17);
             this.labelScript.TabIndex = 13;
@@ -119,19 +116,84 @@
             // 
             this.btnExecute.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExecute.Location = new System.Drawing.Point(481, 481);
+            this.btnExecute.Location = new System.Drawing.Point(170, 344);
             this.btnExecute.Name = "btnExecute";
             this.btnExecute.Size = new System.Drawing.Size(103, 33);
             this.btnExecute.TabIndex = 14;
             this.btnExecute.Text = "Execute";
             this.btnExecute.UseVisualStyleBackColor = false;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // richTextBoxSql
+            // 
+            this.richTextBoxSql.Location = new System.Drawing.Point(62, 383);
+            this.richTextBoxSql.Name = "richTextBoxSql";
+            this.richTextBoxSql.Size = new System.Drawing.Size(507, 103);
+            this.richTextBoxSql.TabIndex = 15;
+            this.richTextBoxSql.Text = "";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(723, 63);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(117, 39);
+            this.btnRefresh.TabIndex = 16;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnAddTable
+            // 
+            this.btnAddTable.Location = new System.Drawing.Point(659, 383);
+            this.btnAddTable.Name = "btnAddTable";
+            this.btnAddTable.Size = new System.Drawing.Size(85, 23);
+            this.btnAddTable.TabIndex = 17;
+            this.btnAddTable.Text = "Add Table";
+            this.btnAddTable.UseVisualStyleBackColor = true;
+            this.btnAddTable.Click += new System.EventHandler(this.btnAddTable_Click);
+            // 
+            // btnDeleteTable
+            // 
+            this.btnDeleteTable.Location = new System.Drawing.Point(659, 412);
+            this.btnDeleteTable.Name = "btnDeleteTable";
+            this.btnDeleteTable.Size = new System.Drawing.Size(85, 23);
+            this.btnDeleteTable.TabIndex = 18;
+            this.btnDeleteTable.Text = "Delete Table";
+            this.btnDeleteTable.UseVisualStyleBackColor = true;
+            this.btnDeleteTable.Click += new System.EventHandler(this.btnDeleteTable_Click);
+            // 
+            // labelLog
+            // 
+            this.labelLog.AutoSize = true;
+            this.labelLog.BackColor = System.Drawing.Color.Transparent;
+            this.labelLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLog.Location = new System.Drawing.Point(59, 504);
+            this.labelLog.Name = "labelLog";
+            this.labelLog.Size = new System.Drawing.Size(35, 17);
+            this.labelLog.TabIndex = 19;
+            this.labelLog.Text = "Log";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(62, 529);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(507, 61);
+            this.richTextBoxLog.TabIndex = 20;
+            this.richTextBoxLog.Text = "";
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(901, 550);
+            this.ClientSize = new System.Drawing.Size(893, 617);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.labelLog);
+            this.Controls.Add(this.btnDeleteTable);
+            this.Controls.Add(this.btnAddTable);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.richTextBoxSql);
             this.Controls.Add(this.btnExecute);
             this.Controls.Add(this.labelScript);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.labelSelectTable);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.btnConnect);
@@ -152,9 +214,14 @@
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.Label labelSelectTable;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelScript;
         private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.RichTextBox richTextBoxSql;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnAddTable;
+        private System.Windows.Forms.Button btnDeleteTable;
+        private System.Windows.Forms.Label labelLog;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
     }
 }
 
--- DatabaseConnection/Form1.cs
+++ DatabaseConnection/Form1.cs
@@ -93,6 +93,7 @@ namespace DatabaseConnection
                 {
                     connection.Open();
                     MessageBox.Show("Connection successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LogMessage("Connection successful!");
 
                     // Tablo isimlerini al ve ComboBox'a yükle
                     FetchTableNames(connection);
@@ -100,8 +101,130 @@ namespace DatabaseConnection
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Connection failed: {ex.Message}\n\n{ex.StackTrace}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    LogMessage($"Connection failed: {ex.Message}");
                 }
             }
         }
+
+        //SQL
+        // SQL Komutunu çalıştıran metot
+        private void ExecuteSqlCommand(SqlConnection connection)
+        {
+            string query = richTextBoxSql.Text; // TextBox'taki SQL komutunu alıyoruz
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                MessageBox.Show("Please enter a valid SQL command.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+
+                LogMessage($"Executing SQL Command: {query}");
+
+                if (query.Trim().StartsWith("SELECT", StringComparison.OrdinalIgnoreCase))
+                {
+                    // Eğer bir SELECT sorgusuysa verileri getirip DataGridView'e gösteriyoruz
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    dataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    // SELECT dışındaki komutlar için (INSERT, UPDATE, DELETE, vb.) ExecuteNonQuery kullanıyoruz
+                    int affectedRows = command.ExecuteNonQuery();
+                    MessageBox.Show($"{affectedRows} rows affected.", "Execution Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LogMessage($"{affectedRows} rows affected by the command.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"SQL execution failed: {ex.Message}\n\n{ex.StackTrace}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                LogMessage($"SQL execution failed: {ex.Message}");
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string connectionString = txtConnectionString.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    ExecuteSqlCommand(connection); // SQL komutunu çalıştır
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Connection failed: {ex.Message}\n\n{ex.StackTrace}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            string connectionString = txtConnectionString.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // ComboBox'taki tablo isimlerini temizle
+                    cmbTables.Items.Clear();
+
+                    // Veritabanından tablo isimlerini yeniden al ve ComboBox'a ekle
+                    FetchTableNames(connection);
+
+                    MessageBox.Show("Data refreshed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Data refresh failed: {ex.Message}\n\n{ex.StackTrace}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnAddTable_Click(object sender, EventArgs e)
+        {
+            // SQL scriptini RichTextBox'a ekleyelim
+            string addTableScript = @"
+            CREATE TABLE DatabaseDocument
+            (
+                DocumentID INT IDENTITY(1,1) PRIMARY KEY,   -- Benzersiz belge kimliği (otomatik artan)
+                Title NVARCHAR(255) NOT NULL,               -- Belge başlığı
+                Content NVARCHAR(MAX) NOT NULL              -- Belge içeriği
+            );";
+
+            richTextBoxSql.Text = addTableScript; // Scripti RichTextBox'a ekle
+        }
+
+        private void btnDeleteTable_Click(object sender, EventArgs e)
+        {
+            // SQL scriptini RichTextBox'a ekleyelim
+            string deleteTableScript = "DROP TABLE DatabaseDocument;";
+
+            richTextBoxSql.Text = deleteTableScript; // Scripti RichTextBox'a ekle
+        }
+
+
+        //Log Messages
+        private void LogMessage(string message)
+        {
+            // Log mesajını zaman damgasıyla RichTextBox'a ekle
+            richTextBoxLog.AppendText($"{DateTime.Now}: {message}\r\n");
+
+            // En son log mesajını görünür yapmak için imleci en sona kaydır
+            richTextBoxLog.SelectionStart = richTextBoxLog.Text.Length;
+            richTextBoxLog.ScrollToCaret();
+        }
+
+
     }
 }
